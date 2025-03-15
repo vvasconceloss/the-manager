1632852550a299bc7bf6a68629c8144ccf9544e5
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2025-03-15 13:06:31 +0000

    feat: create migration files to populate the database
    - popularise the database with data from the nations and the premier league (2024/25)

---
+++ backend/database/migrations/001_seed_season.sql
@@ -0,0 +1,2 @@
+INSERT INTO season (id, name, start_date, end_date) VALUES
+(1, '2024/25', '2024-08-01', '2025-05-31');
---
+++ backend/database/migrations/005_seed_competition.sql
@@ -0,0 +1,4 @@
+INSERT INTO competition (id, name, type, continent, season_id) VALUES
+(1, 'Premier League', 'League', 'Europe', 1),
+(2, 'FA Cup', 'Cup', 'Europe', 1),
+(3, 'EFL Cup', 'Cup', 'Europe', 1);
