5e6e8ad539e2242b3001dd19f6ced91c14ddb019
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2025-03-15 21:29:49 +0000

    feat: create typing for the player
    - create a type for the ‘player’ entity
    - create a typing for the ‘player_contract’ entity

---
+++ backend/src/types/player.d.ts
@@ -0,0 +1,42 @@
+export type Player = {
+  id: number,
+  first_name: string,
+  last_name: string,
+  birth_date: string,
+  position: string,
+  market_value: number,
+  current_ability: number,
+  potential_ability: number,
+  overall: number,
+  finishing: number,
+  crossing: number,
+  dribbling: number
+  heading: number,
+  tackling: number,
+  marking: number,
+  passing: number,
+  free_kick: number,
+  acceleration: number,
+  agility: number,
+  strength: number,
+  jumping: number,
+  vision: number,
+  decision: number,
+  positioning: number,
+  antecipation: number,
+  aggression: number,
+  reflexes: number,
+  handling: number,
+  diving: number,
+  fatigue: number,
+  nation_id: number
+}
+
+export type PlayerContract = {
+  id: number,
+  salary: number,
+  start_date: string,
+  end_date: string,
+  player_id: number,
+  club_id: number
+}
