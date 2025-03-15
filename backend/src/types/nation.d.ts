27569382317056c4633e8dd8f742b5516ee6eb43
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2025-03-15 21:40:42 +0000

    feat: create a typing for the nation

---
+++ backend/src/types/nation.d.ts
@@ -0,0 +1,11 @@
+export type Nation = {
+  id: number,
+  name: string,
+  code: string,
+  continent: string,
+  ranking: number,
+  youth_rating: number,
+  national_team: number,
+  image_federation: string,
+  image_flag: string
+}
