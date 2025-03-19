d6bc3068fd01852c1f349f431281706cecb78506
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2025-03-19 22:19:43 +0000

    feat: create a type for the club

---
+++ backend/src/types/club.d.ts
@@ -0,0 +1,12 @@
+export type Club = {
+  name: string,
+  foundend_year: number,
+  reputation: number,
+  balance: number,
+  budget: number,
+  salaries: number,
+  is_nation: number,
+  image_logo: string,
+  nation_id: number,
+  stadium_id: number,
+}
