cdc4966e03be6f6b01162670fd870b4cba0eaa07
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2025-03-19 11:35:16 +0000

    feat: create a file for services related to nations
    - - create a function that fetches all the youth rating values for each nation

---
+++ backend/src/services/nation.service.ts
@@ -0,0 +1,15 @@
+import NationModel from "../models/nation.model";
+
+class NationService {
+  private static youthRating: Record<string, number> = {};
+
+  static async getNationsYouthRating(): Promise<Record<string, number>> {
+    const nations = await NationModel.fetchAll();
+
+    for (const nation of nations) this.youthRating[nation.name] = nation.youth_rating;
+
+    return this.youthRating;
+  }
+}
+
+export default NationService;
