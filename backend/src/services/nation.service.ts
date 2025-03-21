7476ca33d8771dc09b80456d7196dee8dc310fa2
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2025-03-21 22:05:43 +0000

    feat: create services to search one and many nations (data)

--- backend/src/services/nation.service.ts
+++ backend/src/services/nation.service.ts
@@ -2,10 +2,13 @@ import NationModel from "../models/nation.model";
 
 class NationService {
   private static youthRating: Record<string, number> = {};
+  
+  static async getAllNationInformation() { return await NationModel.fetchAll(); }
+
+  static async getNationInformation(nation: string) { return await NationModel.fetchInformation(nation); }
 
   static async getNationsYouthRating(): Promise<Record<string, number>> {
     const nations = await NationModel.fetchAll();
-
     for (const nation of nations) this.youthRating[nation.name] = nation.youth_rating;
 
     return this.youthRating;
