c6f25f3d0306a46c3c31ad6021763f731457cd82
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2025-03-21 22:08:53 +0000

    feat: create a file with the positions needed for each club at the start

---
+++ backend/src/data/player-positions.ts
@@ -0,0 +1,6 @@
+const positionDistribution: Record<string, number> = {
+  "GK": 2, "CB": 4, "LB": 3, "RB": 3, "CDM": 2,
+  "CM": 4, "CAM": 2, "LW": 2, "RW": 2, "ST": 2
+};
+
+export default positionDistribution;
