d50a83cfbf395e85c014b296ce0af32c89b600a2
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-12-10 11:32:46 +0000

    feat(competition): create an entity to represent a competition

---
+++ crates/core/src/competition/entities/competition.rs
@@ -0,0 +1,32 @@
+use crate::{
+    competition::value_objects::competition::{comp_type::CompetitionType, name::CompetitionName},
+    shared::value_objects::reputation::Reputation,
+};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Competition {
+    pub id: u64,
+    pub nation_id: u64,
+    pub name: CompetitionName,
+    pub reputation: Reputation,
+    pub competition_type: CompetitionType,
+}
+
+impl Competition {
+    pub fn new(
+        id: u64,
+        nation_id: u64,
+        name: CompetitionName,
+        reputation: Reputation,
+        competition_type: CompetitionType,
+    ) -> Self {
+        Competition {
+            id,
+            nation_id,
+            name,
+            reputation,
+            competition_type,
+        }
+    }
+}
