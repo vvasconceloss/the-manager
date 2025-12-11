4e7c24287467ce71bfb7099b79faf7f110f98bfb
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-12-11 21:24:32 +0000

    feat(context): create a struct to define the state (current date) of the simulation

---
+++ crates/core/src/context/simulation.rs
@@ -0,0 +1,14 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct SimulationState {
+    pub day: u8,
+    pub month: u8,
+    pub year: u64,
+}
+
+impl SimulationState {
+    pub fn new(day: u8, month: u8, year: u64) -> Self {
+        SimulationState { day, month, year }
+    }
+}
