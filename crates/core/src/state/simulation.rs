eacf514fdc00e808c5169b49a7ff29c014f1c184
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-12-12 11:05:18 +0000

    feat(state): add time manipulation methods to the simulation state

--- crates/core/src/state/simulation.rs
+++ crates/core/src/state/simulation.rs
@@ -10,7 +10,19 @@ impl SimulationState {
         SimulationState { date }
     }
 
+    pub fn set_date(&mut self, date: NaiveDate) {
+        self.date = date;
+    }
+
+    pub fn formatted_date(&self) -> String {
+        self.date.format("%Y-%m-%d").to_string()
+    }
+
     pub fn advance_day(&mut self) {
         self.date = self.date.succ_opt().unwrap();
+    }
+
+    pub fn is_before(&self, other: NaiveDate) -> bool {
+        self.date < other
     }
 }
