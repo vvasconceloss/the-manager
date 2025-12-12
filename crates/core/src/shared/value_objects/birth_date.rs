cb43ff7d3a30a5272e9dc8a717534cbbc326c3ab
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-12-12 13:20:14 +0000

    fix(shared): correct birth date format to ISO

--- crates/core/src/shared/value_objects/birth_date.rs
+++ crates/core/src/shared/value_objects/birth_date.rs
@@ -6,7 +6,7 @@ pub struct BirthDate(pub NaiveDate);
 
 impl BirthDate {
     pub fn new(birth_date_str: &str) -> Result<Self, CoreError> {
-        let birth_date = NaiveDate::parse_from_str(birth_date_str, "%d%-%m-%Y")
+        let birth_date = NaiveDate::parse_from_str(birth_date_str, "%Y-%m-%d")
             .map_err(|_e| CoreError::ValidationFailed("Birth date is invalid".to_string()))?;
 
         Ok(BirthDate(birth_date))
