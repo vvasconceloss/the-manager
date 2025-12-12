55f69498df0d2029ae6b2db3b4ea3bb9fe125337
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-12-12 11:15:27 +0000

    feat(state): create a state to centralize the current state of the game

---
+++ crates/core/src/state/game.rs
@@ -0,0 +1,54 @@
+use crate::{
+    club::entities::{club::Club, player::Player},
+    competition::entities::competition::Competition,
+    state::simulation::SimulationState,
+};
+use chrono::NaiveDate;
+
+#[derive(Debug, Clone)]
+pub struct GameState {
+    pub clubs: Vec<Club>,
+    pub players: Vec<Player>,
+    pub competitions: Vec<Competition>,
+
+    pub simulation: SimulationState,
+}
+
+impl GameState {
+    pub fn new(start_date: NaiveDate) -> Self {
+        GameState {
+            clubs: Vec::new(),
+            players: Vec::new(),
+            competitions: Vec::new(),
+            simulation: SimulationState::new(start_date),
+        }
+    }
+
+    pub fn advance_day(&mut self) {
+        self.simulation.advance_day();
+
+        self.run_daily_tasks();
+
+        self.process_matches_for_day();
+
+        if self.is_end_of_season() {
+            self.handle_season_end();
+        }
+    }
+
+    pub fn run_daily_tasks(&mut self) {
+        todo!()
+    }
+
+    pub fn process_matches_for_day(&mut self) {
+        todo!()
+    }
+
+    pub fn is_end_of_season(&self) -> bool {
+        false
+    }
+
+    pub fn handle_season_end(&mut self) {
+        todo!()
+    }
+}
