f5ff3865feaf5cb0da0f06d7f344eb3d556908d4
vvasconceloss <vvasconcelos.dev@gmail.com>
2026-05-07 15:15:14 +0100

    feat: add AppState to Tauri's state management

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,13 +1,18 @@
+use crate::state::AppState;
+
 pub mod commands;
 pub mod db;
 pub mod domain;
 pub mod engine;
+pub mod errors;
+pub mod state;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .manage(AppState::default())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
-        .expect("failda to start");
+        .expect("failed to start the application");
 }
