658c591cbe6cd0cd7b39275e40c3d2073d6a9574
vvasconceloss <vvasconcelos.dev@gmail.com>
2026-05-12 16:11:34 +0100

    feat: configure the Tanstack provider router

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider } from "@tanstack/react-router";
 
 import "./index.css";
+import { router } from "./router";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode></React.StrictMode>,
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
