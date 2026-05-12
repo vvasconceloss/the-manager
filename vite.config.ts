d9d0da2806759303fde22fe98a4e07db79ef1003
vvasconceloss <vvasconcelos.dev@gmail.com>
2026-05-12 16:05:15 +0100

    feat: configure Tanstack to work with vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 const host = process.env.TAURI_DEV_HOST as string | undefined;
 
 export default defineConfig(async () => ({
-  plugins: [react()],
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    react(),
+  ],
   define: {
     __DEV__: process.env.NODE_ENV !== "production",
   },
