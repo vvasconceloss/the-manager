77235b138faad3710e849ab51c6af4df66b63226
vvasconceloss <vvasconcelos.dev@gmail.com>
2026-05-16 14:45:50 +0100

    chore: add fonts, assets, docs, and update dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import { RouterProvider } from "@tanstack/react-router";
 
 import "./index.css";
 import { router } from "./router";
+import { Toaster } from "@/components/ui/sonner";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <Toaster position="bottom-right" richColors />
   </React.StrictMode>,
 );
